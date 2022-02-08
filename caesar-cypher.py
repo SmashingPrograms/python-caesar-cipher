30b59ceb02e02eca7adf93cdde5e5e2222509d44
SmashingPrograms <smashingprograms@gmail.com>
2022-02-08 11:07:57 -0500

    Prompts

--- caesar-cypher.py
+++ caesar-cypher.py
@@ -4,19 +4,23 @@ lowercase_letters_string = uppercase_letters_string.lower()
 uppercase_letters = list(uppercase_letters_string)
 lowercase_letters = list(lowercase_letters_string)
 
-test_string = "In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example,"
+# test_string = "In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example,"
 
-shift_amount = 505505050505
+test_string = input("String to test: ")
+shift_amount = input("Pick a shift value: ")
+
+try:
+  shift_amount = int(shift_amount)
+except ValueError:
+  print("Please enter an actual number!")
+  exit()
 
 if shift_amount > 26:
   shift_amount = shift_amount % 26
 if shift_amount < 0:
   if shift_amount < -26:
     shift_amount = shift_amount % -26
-    print(shift_amount, "negative")
   shift_amount = 26 + shift_amount
-
-print(shift_amount)
 
 def caesar_cipher(string):
   new_list = []
